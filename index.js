cdfaef99c5643a18062d5391927f34aaf5871e1b
beso <besopoladashvili@gmail>
2022-04-15 10:38:24 +0400

    Image Slider

---
+++ index.js
@@ -0,0 +1,39 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftArrow = document.getElementById('left')
+const rightArrow = document.getElementById('right')
+
+let activeSlide = 0
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  setBgToBody()
+})
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+rightArrow.addEventListener('click', () => {
+  activeSlide++
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0
+  }
+  setBgToBody()
+  setActiveSlide()
+})
+
+
+leftArrow.addEventListener('click', () => {
+  activeSlide--
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1
+  }
+  setBgToBody()
+  setActiveSlide()
+})
+
+function setActiveSlide() {
+  slides.forEach(slide => slide.classList.remove('active'))
+  slides[activeSlide].classList.add('active')
+}
